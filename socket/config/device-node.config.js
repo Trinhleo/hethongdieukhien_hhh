2bb65bdfbea784f81bdc7630f4afcd99187d8022
trinhleo <hnkt2907@gmail.com>
2017-05-06 23:45:33 +0700

    asdsa

--- socket/config/device-node.config.js
+++ socket/config/device-node.config.js
@@ -58,32 +58,39 @@ module.exports = function (io) {
             socket.emit('deviceNodeData', info);
         });
         function sendData(data) {
-            var myData = data ? "on" : "off";
             appClient.publishDeviceCommand("esp8266", "device01", "control", "txt", data);
         }
         socket.on('fan1', function (data) {
-            sendData(data);
+            var myData = data ? "onfan1" : "offfan1";
+            sendData(myData);
         })
         socket.on('fan2', function (data) {
-            sendData(data);
+            var myData = data ? "onfan2" : "offfan2";
+            sendData(myData);
         })
         socket.on('pumb1', function (data) {
-            sendData(data);
+            var myData = data ? "onpump1" : "offpump1";
+            sendData(myData);
         })
         socket.on('pumb2', function (data) {
-            sendData(data);
+            var myData = data ? "onpump2" : "offpump2";
+            sendData(myData);
         })
         socket.on('boiler1', function (data) {
-            sendData(data);
+            var myData = data ? "onboiler1" : "offboiler1";
+            sendData(myData);
         })
         socket.on('boiler2', function (data) {
-            sendData(data);
+            var myData = data ? "onboiler2" : "offboiler2";
+            sendData(myData);
         })
         socket.on('light', function (data) {
-            sendData(data);
+            var myData = data ? "onlight" : "offlight";
+            sendData(myData);
         })
         socket.on('mode', function (data) {
-            sendData(data);
+            var myData = data ? "manual" : "auto";
+            sendData(myData);
         })
         socket.on('disconnect', function onDisconnect() {
             socket.leaveAll()
