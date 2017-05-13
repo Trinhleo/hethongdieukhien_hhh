d4331cb2d94dac3cf5230061efb70cf228c64f65
trinhleo <hnkt2907@gmail.com>
2017-05-13 17:55:36 +0700

    asdsd

--- socket/config/device-node.config.js
+++ socket/config/device-node.config.js
@@ -51,43 +51,43 @@ module.exports = function (io) {
             appClient.publishDeviceCommand("esp8266", deviceId, "control", "txt", data);
         }
         socket.on('fan1', function (data) {
-            var myData = data ? "onfan1" : "offfan1";
+            var myData = data ? "of1" : "ff1";
             sendData(myData, "device01");
         })
         socket.on('fan2', function (data) {
-            var myData = data ? "onfan2" : "offfan2";
+            var myData = data ? "of2" : "ff2";
             sendData(myData, "device02");
         })
         socket.on('pumb1', function (data) {
-            var myData = data ? "onpump1" : "offpump1";
+            var myData = data ? "op1" : "fp1";
             sendData(myData, "device01");
         })
         socket.on('pumb2', function (data) {
-            var myData = data ? "onpump2" : "offpump2";
+            var myData = data ? "op2" : "fp2";
             sendData(myData, "device02");
         })
         socket.on('boiler1', function (data) {
-            var myData = data ? "onboiler1" : "offboiler1";
+            var myData = data ? "ob1" : "fb1";
             sendData(myData, "device01");
         })
         socket.on('boiler2', function (data) {
-            var myData = data ? "onboiler2" : "offboiler2";
+            var myData = data ? "ob2" : "fb2";
             sendData(myData, "device02");
         })
         socket.on('light1', function (data) {
-            var myData = data ? "onlight1" : "offlight1";
+            var myData = data ? "ol1" : "fl1";
             sendData(myData, "device01");
         })
         socket.on('light2', function (data) {
-            var myData = data ? "onlight2" : "offlight2";
+            var myData = data ? "ol2" : "fl2";
             sendData(myData, "device02");
         })
         socket.on('mode1', function (data) {
-            var myData = data ? "manual1" : "auto1";
+            var myData = data ? "ma1" : "au1";
             sendData(myData, "device01");
         })
         socket.on('mode2', function (data) {
-            var myData = data ? "manual2" : "auto2";
+            var myData = data ? "ma2" : "au2";
             sendData(myData, "device02");
         })
         socket.on('disconnect', function onDisconnect() {
